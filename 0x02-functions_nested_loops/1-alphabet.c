9afb47ad648b1d93979c13d02245364209bbff0b
Hamza Mahjabi <hamzamahjabi@gmail.com>
2023-10-10 08:24:51 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
- *		the alphabet a - z
+ *	the alphabet a - z
+ *	Return: 0 (success)
 */
-
 int print_alphabet(void)
 {
 	int ch;
@@ -12,4 +12,5 @@ int print_alphabet(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
+	return (0);
 }
