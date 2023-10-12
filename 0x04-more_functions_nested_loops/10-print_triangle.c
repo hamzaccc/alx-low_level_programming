87d498dcc8bf47f9b2afb65304793d9935fff98f
Hamza Mahjabi <hamzamahjabi@gmail.com>
2023-10-12 08:19:07 -0700

    saka

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_tringle - prints a tringle
+ *
+ * @size: size of the triangle
+ *
+ * Return: 0 (success)
+*/
+
+void print_triangle(int size)
+{
+	int hgt, base;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (hgt = 1; hgt <= size; hgt++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((hgt + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
