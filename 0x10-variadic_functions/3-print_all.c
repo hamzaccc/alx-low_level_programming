f5fa5dac3028685b8f1a7037205f573876157d14
Hamza Mahjabi <hamzamahjabi@gmail.com>
2023-11-09 06:00:25 -0800

    walo

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -76,7 +76,7 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == tokens[j].token[0])
 			{
-				tokens[j].format(separator, ap);
+				tokens[j].f(separator, ap);
 				separator = ", ";
 			}
 			j++;
